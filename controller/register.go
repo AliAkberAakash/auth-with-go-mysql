6c036a343ec25572ac4b702ce0dcc5372dc137ad
ALIAKBERAAKASH <ali.akber@brainstation-23.com>
2022-04-26 08:50:19 +0600

    implement bcrypt hash for storing passwords

--- controller/register.go
+++ controller/register.go
@@ -1,11 +1,27 @@
 package controller
 
-import "github.com/AliAkberAakash/auth-with-go-mysql/model"
+import (
+	"errors"
+
+	"github.com/AliAkberAakash/auth-with-go-mysql/model"
+	"golang.org/x/crypto/bcrypt"
+)
 
 var Users []model.User
 
-func Register(user model.User) {
+func Register(user model.User) error {
 	if user.IsValid() {
+
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+		if err != nil {
+			return err
+		}
+
+		user.Password = string(passwordHash)
 		Users = append(Users, user)
+		return nil
 	}
+
+	return errors.New("Invalid user data")
 }
